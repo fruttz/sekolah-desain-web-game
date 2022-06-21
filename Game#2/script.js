e132f4a7b2cbf03bc52fd819332375b678182038
fruttz <dito.arjwasa@gmail.com>
2022-06-21 17:27:44 +0700

    starting on game 2

---
+++ Game#2/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', function() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height= 500;
+
+    class Snake {
+
+    }
+
+    class Fruit {
+
+    }
+
+    class InputHandler {
+
+    }
+
+    class StateManager {
+
+    }
+
+    function spawnFruit() {
+
+    }
+
+    function displayText(context){
+
+    }
+
+    //MAIN GAME
+    
+
+
+
+});
