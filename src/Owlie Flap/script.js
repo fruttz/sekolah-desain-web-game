22cfab4d7a6933d5009a4d325d2eab1ab0c45f8b
fruttz <dito.arjwasa@gmail.com>
2022-07-26 14:32:25 +0700

    added assets to game 1

--- src/Owlie Flap/script.js
+++ src/Owlie Flap/script.js
@@ -4,10 +4,10 @@ window.addEventListener('load', function(){
     const gameContext = game.getContext('2d');
     const menu = document.getElementById('menu');
     const gameOverUI = document.getElementById('gameoverUI');
-    menu.style.width = "1080px";
-    menu.style.height = "1920px";
-    game.width = 1080;
-    game.height = 1920;
+    menu.style.width = "1024px";
+    menu.style.height = "1024px";
+    game.width = 768;
+    game.height = 1024;
    
     //UI ELEMENTS
     function showMainMenu(){
@@ -63,19 +63,22 @@ window.addEventListener('load', function(){
     let gameOver = false;
     
 
+    const playerImage = new Image();
+    playerImage.src = ("img/player.png");
     class Player {
         constructor(gameWidth, gameHeight){
             this.gameWidth = gameWidth;
             this.gameHeight = gameHeight;
-            this.width = 100;
-            this.height = 100;
+            this.width = 80;
+            this.height = 75;
             this.x = 20;
             this.y = this.gameHeight/2;
-            this.speed = 10;
+            this.image = playerImage;
+            this.speed = 5;
         }
         draw(context){
             context.fillStyle = 'black';
-            context.fillRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.x, this.y, this.width, this.height)
 
         }
         update(input){
@@ -83,9 +86,9 @@ window.addEventListener('load', function(){
             if (!gameInitialState) this.y += this.speed;
 
             if ((input.keys.indexOf('click') > -1) || (input.keys.indexOf('touch') > -1)){
-                this.speed = -20;
+                this.speed = -10;
             } else {
-                this.speed = 18
+                this.speed = 13
             }
 
             //boundaries
@@ -103,25 +106,33 @@ window.addEventListener('load', function(){
 
     }
 
+    const obstacleTopImage = new Image();
+    const obstacleBottomImage = new Image();
+    obstacleTopImage.src = ("img/obstacleDOWN.png");
+    obstacleBottomImage.src = ("img/obstacleUP.png");
     class Obstacle {
         constructor(gameWidth, gameHeight){
             this.gameWidth = gameWidth;
             this.gameHeight = gameHeight;
-            this.top = (Math.random() * this.gameHeight/3) + 200;
-            this.bottom = (Math.random() * this.gameHeight/3) + 200;
-            this.width = 100;
+            this.top = (Math.random() * this.gameHeight/3) + 100;
+            this.bottom = (Math.random() * this.gameHeight/3) + 100;
+            this.width = obstacleBottomImage.width;
             this.x = this.gameWidth;
+            this.topImage = obstacleTopImage;
+            this.bottomImage = obstacleBottomImage;
             this.markedForDelete = false;
         }
 
         draw(context){
             context.fillStyle = "red";
-            context.fillRect(this.x, 0, this.width, this.top);
-            context.fillRect(this.x, this.gameHeight - this.bottom, this.width, this.bottom);
+            context.drawImage(this.topImage, this.x, 0, this.width, this.top);
+            context.drawImage(this.bottomImage, this.x, this.gameHeight - this.bottom, this.width, this.bottom)
         }
         update(player){
             //move
             this.x -= 10;
+            var xOffset = 3;
+            var yOffset = 10;
 
             //delete after passing boundary and add score
             if (this.x < 0 - this.width) {
@@ -131,12 +142,35 @@ window.addEventListener('load', function(){
 
             //collision detection
             if (player.x < this.x + this.width &&
-                player.x + player.width > this.x &&
-                ((player.y < 0 + this.top && player.y + player.height > 0) || 
-                (player.y + player.height > this.gameHeight - this.bottom && 
-                player.y + player.height < this.gameHeight))){
+                player.x + player.width - xOffset> this.x + this.width/2 &&
+                ((player.y + yOffset < 0 + this.top && player.y + player.height > 0) || 
+                (player.y + yOffset > this.gameHeight - this.bottom && 
+                player.y + player.height + yOffset < this.gameHeight))){
                     gameOver = true;
                 }
+        }
+    }
+
+    const backgroundImage = new Image();
+    backgroundImage.src = ("img/bg.png");
+    class Background {
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.image = backgroundImage;
+            this.width = 1707;
+            this.height = 1024;
+            this.x = 0;
+            this.y = 0;
+            this.speed = 3;
+        }
+        draw(context){
+            context.drawImage(this.image, this.x, this.y,);
+            context.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+        }
+        update(){
+            this.x -= this.speed;
+            if (this.x < 0 - this.width) this.x = 0;
         }
     }
 
@@ -187,14 +221,14 @@ window.addEventListener('load', function(){
         //score text
         context.textAlign = "left";
         context.fillStyle = "black";
-        context.font = "bold 40px Monsterrat";
+        context.font = "bold 20px Monsterrat";
         context.fillText("Score: " + score, 20, 50);
 
         //initial game text
         if(gameInitialState){
             context.textAlign = "center";
             context.fillStyle = "black";
-            context.font = "bold 70px Monsterrat";
+            context.font = "bold 30px Monsterrat";
             context.fillText("TOUCH ANYWHERE TO START", game.width/2, 200);
         }
 
@@ -217,6 +251,7 @@ window.addEventListener('load', function(){
     //MAIN FUNCTION
     const input = new InputHandler();
     const player = new Player(game.width, game.height);
+    const background = new Background(game.width, game.height);
     let lastTime = 0;
     let obstacleTimer = 0;
     let obstacleInterval = 300;
@@ -225,10 +260,14 @@ window.addEventListener('load', function(){
         const deltaTime = timeStamp - lastTime;
         lastTime = timeStamp;
         gameContext.clearRect(0,0,game.width, game.height);
+        background.draw(gameContext);
+        if (!gameInitialState) {
+            background.update();
+            spawnObstacle(deltaTime);
+        }
+        displayText(gameContext);
         player.draw(gameContext);
         player.update(input);
-        if (!gameInitialState) spawnObstacle(deltaTime);
-        displayText(gameContext);
         if (!gameOver) requestAnimationFrame(animate);
 
     }
